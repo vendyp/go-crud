942a076a608dc0eaba868af6684d229b208dadab
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2023-08-04 19:49:15 +0700

    add mod, and main go for entrypoint

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+func main() {
+	fmt.Println("Running web server on port 5000")
+
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.Logger.Fatal(e.Start(":5000"))
+}
