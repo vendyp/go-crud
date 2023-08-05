a9ded9fcdc99d790e6bc9c99a0cf791f32a957be
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2023-08-05 10:02:52 +0700

    minor update

--- main.go
+++ main.go
@@ -5,10 +5,28 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
+	"gorm.io/driver/sqlserver"
+	"gorm.io/gorm"
 )
+
+type User struct {
+	gorm.Model
+	Name string `gorm:"size:100"`
+}
 
 func main() {
 	fmt.Println("Running web server on port 5000")
+
+	//db migrations
+	connString := "sqlserver://sa:Password1234@localhost:1433?database=CrudDb"
+	db, err := gorm.Open(sqlserver.Open(connString), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect db")
+	}
+
+	//migrate table user
+	db.AutoMigrate(&User{})
+	//db.AutoMigrate(&Product{}) // error
 
 	e := echo.New()
 
